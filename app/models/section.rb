4c6bfd671ec87b3e2727986fd36d190d7d4a344d
Matthew Meitl <DarkSamus6@gmail.com>
2016-01-23 20:46:20 +0000

    test

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,6 +3,15 @@ class Section < ActiveRecord::Base
   has_many :lessons
 
   include RankedModel
-   ranks :row_order, :with_same => :course_id    
+   ranks :row_order, :with_same => :course_id  
+   
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end    
+
+    return section
+  end 
 
 end
